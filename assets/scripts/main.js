f09883884c3e01f8a7f33801c67af6219574c96f
houkanshan <houkanshan@gmail.com>
2013-06-13 16:42:15 +0800

    parallax first version

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,5 @@
+(function(){
+
 var body = $('body')
 
 // event bind
@@ -8,18 +10,49 @@ $('.fold').each(function(i, e){
   var target = $(e)
     , content = target.find('.bd')
     , height = content.height()
-    , lineCnt = height / 21 | 0
+    , lineHeight = 21
+    , lineCnt = height / lineHeight | 0
     , unfoldButton = target.next('.unfold-outer').find('.unfold')
     , orignText = unfoldButton.text()
 
-    unfoldButton.text(orignText + ' ( 约末'+ lineCnt + '行 )')
+    unfoldButton.text(orignText + ' ( 约'+ lineCnt + '行 )')
 })
 
 
+// init scrollSlow
+//var scroll = new Scroll({
+  //breakElems: $('h2')
+//})
+this.parallax = new Parallax({
+  moveTarget: $('.wrapper'),
+  points: [
+    {
+      offset: 100,
+      velocityRadio: 0.1, // slow
+    }, 
+    {
+      offset: 200,
+      velocityRadio: 0, // stop
+      delay: 400
+    }, 
+    {
+      offset:400,
+      velocityRadio: 1 // 1/2 speed
+    },
+    {
+      offset:600,
+      velocityRadio: 1   // normal speed
+    }
+  ]
+})
 
+
+// helper functions
 function unfold(e) {
   var target = $(e.target).parent()
   
   target.prev('.fold').removeClass('fold')
   target.remove()
 }
+
+}())
